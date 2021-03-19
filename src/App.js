baee2592b453997b31bc17bac364c44ee3748602
evgenii-ivanov87 <ivanov.genya@icloud.com>
2021-03-19 14:15:21 +0200

    работа с раутом

--- src/App.js
+++ src/App.js
@@ -8,39 +8,39 @@ class App extends Component {
   state= {
   films: [],
   }
-  componentDidMount() {
-   this.fetchFilms()
+
+   componentDidMount() {
+    this.fetchFilms()
+   
   }
     
 
- fetchFilms = () => {
+  fetchFilms = () => {
       axios.get(
         'https://api.themoviedb.org/3/trending/all/day?api_key=73aee633aaf5c834566a91020ffc342e')
-        .then(res => {
-          this.setState({ films: [...res.data.results] })
-          
-        })
+        .then(response => {
+          console.log(response.data.results)
+          this.setState({films: [...response.data.results]})
+                  })
+       
     }
   
   
   
   render() {
     
-    const filmIthems = this.state.films
-    console.dir(filmIthems[0])
     
     
     
     return (
       <>
       <h1>Ues</h1>
-
-        {/* {(filmIthems.length > 0) &&
+      {this.state.films.length>0 && <ul>
+          {this.state.films.map((film) => (<li key={film.id}>
+            
+            {film.title} {film.name}</li>))}
+        </ul>}
         
-          < ul >
-        {filmIthems.map(({id,original_title}) =>
-        { <li key={id}> <p>{original_title}</p></li> })}
-      </ul>} */}
       </>
      
   );
